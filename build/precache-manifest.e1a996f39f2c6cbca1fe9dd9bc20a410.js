0c2de1c494ac37a026711520f35a6a1fafcbaeef
Ba-Zen <bazenatlas@gmail.com>
2019-09-28 14:58:28 -0400

    file transfer

---
+++ build/precache-manifest.e1a996f39f2c6cbca1fe9dd9bc20a410.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e2c7e7c0e53f24345c38b23613adc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deb991f25d4243beb0ac",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "deb991f25d4243beb0ac",
+    "url": "/static/js/main.ec0a7c78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
